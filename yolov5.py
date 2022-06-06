be3a2ff49f6dbca6afa20223a9762a8b9c40b1ae
Zalmwel <samuelwkuik@gmail.com>
2022-06-06 18:03:29 +0200

    Updated commented out sections

--- yolov5.py
+++ yolov5.py
@@ -2,14 +2,15 @@ import torch
 import cv2
 import os, os.path
 
-# Rename this to path of YOUR local yolov5 repo
-path_to_local_repo = '/Users/samuelkuik/Desktop/yolov5/yolov5'
+
+path_to_local_repo = 'yolov5-repository/yolov5'
+
 # path=f'{path_to_local_repo}/runs/train/LATEST EXP/weights/best.pt'
-model = torch.hub.load(path_to_local_repo, 'custom', path=f'{path_to_local_repo}/runs/train/exp13/weights/best.pt', source='local')
+model = torch.hub.load(path_to_local_repo, "custom", path=f'{path_to_local_repo}/runs/train/exp15/weights/best.pt', source='local')
 
 # When using the complete dataset:
-# Copy folder 'New Complete set' from onedrive
-# rename to 'complete-set'
+# Copy folder with complete dataset to the root of this repository (~/KNSA_Project/KNSA-training-model/{PLACE DATASET HERE})
+# rename folder to 'complete-set'
 
 # Read all files in folder
 # file_list = os.listdir("complete-set")
@@ -25,6 +26,8 @@ model = torch.hub.load(path_to_local_repo, 'custom', path=f'{path_to_local_repo}
 # for file in file_list:
 #     img = cv2.imread(f"complete-set/{file}")
 #     images.append(img)
+
+# Comment out lines 34-39
 
 model.max_det = 5
 
